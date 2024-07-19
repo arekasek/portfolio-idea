93dcd1002e92c7c5b0c78fc1e851f15985f0cb57
arekasek <110391257+arekasek@users.noreply.github.com>
2024-07-19 18:34:47 +0200

    Update Spline.jsx

--- src/app/Spline.jsx
+++ src/app/Spline.jsx
@@ -3,7 +3,7 @@ import Spline from "@splinetool/react-spline/next";
 export default function SplineScene() {
   return (
     <div className="absolute top-0 left-0 w-full h-full">
-       <Spline
+        <Spline
         scene="https://prod.spline.design/3UFsPs9TuzqJjrKX/scene.splinecode" 
       />
     </div>
