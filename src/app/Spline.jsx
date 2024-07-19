320cdbf06af97d86ed07469c5f04e474ef4edc16
arekasek <110391257+arekasek@users.noreply.github.com>
2024-07-19 18:38:15 +0200

    Update Spline.jsx

--- src/app/Spline.jsx
+++ src/app/Spline.jsx
@@ -4,7 +4,7 @@ export default function SplineScene() {
   return (
     <div className="absolute top-0 left-0 w-full h-full">
         <Spline
-        scene="https://prod.spline.design/3UFsPs9TuzqJjrKX/scene.splinecode" 
+        scene="https://prod.spline.design/3UFsPs9TuzqJjrKX/scene.splinecode"
       />
     </div>
   );
